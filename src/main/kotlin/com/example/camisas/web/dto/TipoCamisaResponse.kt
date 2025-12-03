f13ac69d7a37175d76477770000284921d983077
Javier <j.aguilera@cesjuanpablosegundocadiz.es>
2025-12-04 00:59:08 +0100

    Test: Añade campo de versión para verificar despliegue

--- src/main/kotlin/com/example/camisas/web/dto/TipoCamisaResponse.kt
+++ src/main/kotlin/com/example/camisas/web/dto/TipoCamisaResponse.kt
@@ -3,5 +3,6 @@ package com.example.camisas.web.dto
 data class TipoCamisaResponse(
     val id: Long,
     val nombre: String,
-    val descripcion: String?
+    val descripcion: String? = null,
+    val version: String = "default" // Campo de prueba
 )
